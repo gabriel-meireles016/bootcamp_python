efc028a5b769ac021c6342f0acc92bc112d63063
gabriel-meireles016 <meirelesgabriel016@gmail.com>
2025-07-28 21:40:17 -0400

    Adição das funções do desafio anterior: depositar(), sacar() e extrato()

--- sistema_bancario/desafio.py
+++ sistema_bancario/desafio.py
@@ -1,3 +1,38 @@
+def depositar():
+    valor = float(input("Informe o valor do depósito: "))
+
+    if valor > 0:
+        saldo += valor
+        extrato += f"Depósito de R${valor:.2f}\n"
+    else:
+        print("Operação falhou. Valor inválido.")
+
+def sacar():
+    valor = float(input("Informe o valor do saque: "))
+
+    caso1 = valor > saldo
+    caso2 = valor > limite
+    caso3 = numero_saques >= LIMITE_SAQUES
+    caso4 = valor > 0
+
+    if caso1:
+        print("Operação falhou. Valor maior que o saldo.")
+    elif caso2:
+        print("Operação falhou. Valor de saque maior que o limite.")
+    elif caso3:
+        print("Operação falhou. Limite de saques alcançado.")
+    elif caso4:
+        saldo -= valor
+        extrato += f"Saque de R${valor:.2f}\n"
+        numero_saques += 1
+    else:
+        print("Operação falhou. Valor inválido.")
+
+def extrato():
+    print("Extrato Bancário")
+    print("Sem transações." if not extrato else extrato)
+    print(f"Saldo: R${saldo:.2f}")
+
 menu = """
 
 [1] Depositar
@@ -18,37 +53,11 @@ while True:
     opcao = input(menu)
 
     if opcao == 1:
-        valor = float(input("Informe o valor do depósito: "))
-
-        if valor > 0:
-            saldo += valor
-            extrato += f"Depósito de R${valor:.2f}\n"
-        else:
-            print("Operação falhou. Valor inválido.")
+        depositar()
     elif opcao == 2:
-        valor = float(input("Informe o valor do saque: "))
-
-        caso1 = valor > saldo
-        caso2 = valor > limite
-        caso3 = numero_saques >= LIMITE_SAQUES
-        caso4 = valor > 0
-
-        if caso1:
-            print("Operação falhou. Valor maior que o saldo.")
-        elif caso2:
-            print("Operação falhou. Valor de saque maior que o limite.")
-        elif caso3:
-            print("Operação falhou. Limite de saques alcançado.")
-        elif caso4:
-            saldo -= valor
-            extrato += f"Saque de R${valor:.2f}\n"
-            numero_saques += 1
-        else:
-            print("Operação falhou. Valor inválido.")
+        sacar()
     elif opcao == 3:
-        print("Extrato Bancário")
-        print("Sem transações." if not extrato else extrato)
-        print(f"Saldo: R${saldo:.2f}")
+        extrato()
     elif opcao == 0:
         break
 
